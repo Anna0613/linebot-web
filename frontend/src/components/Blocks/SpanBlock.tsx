ff57aaf133a0038a5326dce422c00c0c960dd7b5
美女 <a0973632289@gmail.com>
2025-05-18 03:20:10 +0800

    先刪還沒改完

--- frontend/src/components/Blocks/SpanBlock.tsx
+++
@@ -1,9 +0,0 @@
-const SpanBlock = () => {
-    return (
-      <div className="w-[120px] h-[40px] bg-[#153F7A] text-white rounded flex items-center justify-center">
-        多樣式文字
-      </div>
-    );
-  };
-  
-  export default SpanBlock;
--- frontend/src/components/Blocks/WidthBlock.tsx
+++
@@ -1,9 +0,0 @@
-const WidthBlock = () => {
-  return (
-    <div className="w-[50px] h-[24px] rounded-full bg-[#CDB4DB] flex items-center justify-center">
-      寬度
-    </div>
-  );
-};
-
-export default WidthBlock;
